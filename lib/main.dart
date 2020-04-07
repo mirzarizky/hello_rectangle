5ea45e4227f60948e636113ccbeb27d0ce2e6168
Mirza Rizky <mirzarizky@gmail.com>
2020-04-07 14:15:45 +0700

    from the scratch hello rectangle

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,4 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Rectangle'),
-        ),
-        body: HelloRectangle(),
-      ),
-    ),
-  );
-}
 
 class HelloRectangle extends StatelessWidget {
   @override
@@ -25,16 +6,27 @@ class HelloRectangle extends StatelessWidget {
     return Center(
       child: Container(
         color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
+        width: 300,
+        height: 500,
         child: Center(
           child: Text(
-            'Hello!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
+            'Hello',
+            style: TextStyle(fontSize: 40),
           ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Title'),
+      ),
+      body: HelloRectangle()
+    ),
+  ));
 }
